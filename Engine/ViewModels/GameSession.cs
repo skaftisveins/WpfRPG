7957ca3b6ed3121e6146ce6d925fb588a0caf69f
Skafti Sveinsson <ss1.h18@nemandi.ntv.is>
2018-12-10 20:53:15 +0000

    Fyrstu skrefin komin..

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Skafti";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+        }
+    }
+}
