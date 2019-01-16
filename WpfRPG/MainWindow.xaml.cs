6418fef0fe53187a58fb3f3574694885c37be8ca
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-01-16 14:54:48 +0000

    Lesson 09.2: Adding the Trade Screen

--- WpfRPG/MainWindow.xaml.cs
+++ WpfRPG/MainWindow.xaml.cs
@@ -52,6 +52,12 @@ namespace WpfRPG
             GameMessages.ScrollToEnd();
         }
 
-        
+        private void OnClick_DisplayTradeScreen(object sender, RoutedEventArgs e)
+        {
+            TradeScreen tradeScreen = new TradeScreen();
+            tradeScreen.Owner = this;
+            tradeScreen.DataContext = _gameSession;
+            tradeScreen.ShowDialog();
+        }
     }
 }
